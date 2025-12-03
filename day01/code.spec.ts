6f0d9d179397c6f943c63c8ed590f102e907ad7e
Joe Danks <joedanks@gmail.com>
2025-12-02 20:46:16 -0600

    complete day 1 part 2

--- day01/code.spec.ts
+++ day01/code.spec.ts
@@ -1,32 +1,53 @@
-import { part1, part2 } from './code';
-import input from './input.json';
+import { part1, part2 } from "./code";
+import input from "./input.json";
 
-describe('Day 01', () => {
+describe("Day 01", () => {
   const testInput = [
-    'L68',
-    'L30',
-    'R48',
-    'L5',
-    'R60',
-    'L55',
-    'L1',
-    'L99',
-    'R14',
-    'L82',
+    "L68",
+    "L30",
+    "R48",
+    "L5",
+    "R60",
+    "L55",
+    "L1",
+    "L99",
+    "R14",
+    "L82",
+  ];
+  const extraTestInput = [
+    "L168",
+    "L30",
+    "R48",
+    "L5",
+    "R60",
+    "L55",
+    "L1",
+    "L99",
+    "R14",
+    "L82",
   ];
 
-  describe('Part 1', () => {
-    it('should solve the example', () => {
+  describe("Part 1", () => {
+    it("should solve the example", () => {
       expect(part1(testInput)).toBe(3);
     });
-    it('should solve the real input', () => {
+    it("should solve the real input", () => {
       expect(part1(input)).toBe(0);
-    })
+    });
   });
 
-  describe('Part 2', () => {
-    it('should solve the example', () => {
-      expect(part2(testInput)).toBe(0); // TODO: Update expected value
+  describe.only("Part 2", () => {
+    it("should solve the example", () => {
+      expect(part2(testInput)).toBe(6);
+    });
+    it("should solve the extra example", () => {
+      expect(part2(extraTestInput)).toBe(7);
+    });
+    it("should solve the real input", () => {
+      const result = part2(input);
+      expect(result).toBeLessThan(6795);
+      expect(result).toBeGreaterThan(6467);
+      expect(result).toBe(0);
     });
   });
 });
--- day01/code.ts
+++ day01/code.ts
@@ -1,9 +1,12 @@
 export function part1(data: string[]): number {
   let zeroCount = 0;
   let pointer = 50;
-  data.forEach(c => {
+  data.forEach((c) => {
     const direction = c.charAt(0);
-    let next = direction == 'L' ? pointer - parseInt(c.substring(1)) : pointer + parseInt(c.substring(1));
+    let next =
+      direction == "L"
+        ? pointer - parseInt(c.substring(1))
+        : pointer + parseInt(c.substring(1));
     next = next % 100;
     if (next < 0) {
       next += 100;
@@ -12,10 +15,38 @@ export function part1(data: string[]): number {
     if (pointer == 0) {
       zeroCount++;
     }
-  })
+  });
   return zeroCount;
 }
 
 export function part2(data: string[]): number {
-  return 0;
+  let zeroCount = 0;
+  let pointer = 50;
+
+  data.forEach((c) => {
+    // console.log(`Command: ${c}`);
+    const direction = c.charAt(0);
+    const ticks = parseInt(c.substring(1));
+    const minTicks = ticks % 100;
+    let next = direction == "L" ? pointer - minTicks : pointer + minTicks;
+    const spins = Math.trunc(ticks / 100);
+    zeroCount += spins;
+    if (next >= 100) {
+      next = next % 100;
+      if (pointer != 0) {
+        zeroCount++;
+      }
+    } else if (next < 0) {
+      next = next % 100;
+      next += 100;
+      if (pointer != 0) {
+        zeroCount++;
+      }
+    } else if (next === 0) {
+      zeroCount++;
+    }
+    pointer = next;
+    // console.log(`Pointer: ${pointer}  Zero Count: ${zeroCount}`);
+  });
+  return zeroCount;
 }
